76859c24d42e2f51b11661566e413baea69e3386
Kimyeonho <kyh0703@nate.com>
2023-12-11 14:23:51 +0900

    Feature: Loading Emotion Css
    emotion css 적용 안되는 현상 수정

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,16 +1,45 @@
-"use client";
+'use client'
 
-import React from "react";
-import { Global } from "@emotion/react";
-import tw, { css, theme, GlobalStyles as BaseStyles } from "twin.macro";
+import React from 'react'
+import { Global } from '@emotion/react'
+import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
 
-const customStyles = css({});
+const customStyles = css({
+  'html, body, #__next': {
+    height: '100%',
+    padding: 0,
+    margin: 0,
+    'font-family': `
+      -apple-system,
+      BlinkMacSystemFont,
+      Segoe UI,
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      Fira Sans,
+      Droid Sans,
+      Helvetica Neue,
+      sans-serif
+    `,
+  },
+  a: {
+    color: 'inherit',
+    'text-decoration': 'none',
+  },
+  '*': {
+    'box-sizing': 'border-box',
+  },
+  body: {
+    padding: '0 40px',
+  },
+})
 
 const GlobalStyles = () => (
   <>
     <BaseStyles />
     <Global styles={customStyles} />
   </>
-);
+)
 
-export default GlobalStyles;
+export default GlobalStyles
