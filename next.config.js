d5db08559d7e1eb635db22582a1af337d5d0aca4
kyh0703 <kyh0703@nate.com>
2023-12-23 19:55:02 +0900

    Feat: Add Markdown components

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withTwin = require('./withTwin')
-const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withTwin(
-  withContentlayer({
-    // Configure 'pageExtensions' to include MDX files
-    pageExtensions: ['js', 'jsx', 'mdx', 'tx', 'tsx'],
-    // Optionally, add any other Next.js config below
-    reactStrictMode: true,
-  }),
-)
+const nextConfig = withTwin({
+  // static page
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // Configure 'pageExtensions' to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'tx', 'tsx'],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+})
 
 module.exports = nextConfig
--- src/components/atoms/Markdown/index.tsx
+++ src/components/atoms/Markdown/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 type MarkdownProps = {
   content?: string
@@ -7,9 +10,27 @@ type MarkdownProps = {
 
 const Markdown = ({ content }: MarkdownProps) => {
   return (
-    <div>
-      <ReactMarkdown>{content}</ReactMarkdown>
-    </div>
+    <ReactMarkdown
+      remarkPlugins={[[remarkGfm]]}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props
+          const match = /language-(\w+)/.exec(className || '')
+
+          return match ? (
+            <SyntaxHighlighter PreTag="div" language={match[1]} style={dracula}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    >
+      {content}
+    </ReactMarkdown>
   )
 }
 
